6b18bd20ef117b55eab0d4925554fdd9db0874e4
Ashen Chathuranga <ktauchathuranga@gmail.com>
2024-04-25 21:57:43 +0530

    commented def pass and user v1

--- func.c
+++ func.c
@@ -21,8 +21,8 @@ void sha256(const char *str, char outputBuffer[65]) {
 }
 
 void login() {
-    char stored_username_hash[] = "ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb";
-    char stored_password_hash[] = "3e23e8160039594a33894f6564e1b1348bbd7a0088d42c4acb73eeaed59c009d";
+    char stored_username_hash[] = "ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb"; // a
+    char stored_password_hash[] = "3e23e8160039594a33894f6564e1b1348bbd7a0088d42c4acb73eeaed59c009d"; // b
 
     char username[100];
     char password[100];
